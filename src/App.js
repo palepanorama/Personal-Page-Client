a69cd775a1066917c7dae16d76dabe0dbf756d9a
palepanorama <matthew.keith.thomas@gmail.com>
2021-08-23 20:23:07 -0500

    adds router and switch plus a nav in app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Home from './components/Home';
+import About from './components/About';
+import Footer from './components/Footer';
+import ErrorPage from './components/ErrorPage';
+import Nav from './components/Nav';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header>I am here!</header>
-    </div>
+
+    <Router>
+      <Nav />
+      <Switch>
+
+        <Route exact path="/">
+          <Home />
+        </Route>
+
+        <Route path="/about">
+          <About />
+        </Route>
+
+        <Route component={ErrorPage}/>
+      </Switch>
+      <Footer />
+    </Router>
   );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div>
+            we're home now
+        </div>
+    )
+}
+
+export default Home
